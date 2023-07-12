1c3b3158a7dcea2bb57f08e7e7c6a94416ab2129
marie-ming <44185091+marie-ming@users.noreply.github.com>
2023-07-12 18:44:33 +0900

    Feat : 에러페이지 생성

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from 'App';
 import Issue from 'pages/Issues';
 import IssueDetail from 'pages/IssueDetail';
+import ErrorPage from 'pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
