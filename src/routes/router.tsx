38b2cbb1e0c2390d3edd02f8ef12c622f3c1e560
marie-ming <44185091+marie-ming@users.noreply.github.com>
2023-07-13 02:10:54 +0900

    Feat : 로딩페이지생성 및 적용

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,13 +1,20 @@
+import React, { Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App, { loader as rootLoader } from 'App';
-import IssuesPage from 'pages/IssuesPage';
-import IssueDetailPage from 'pages/IssueDetailPage';
 import ErrorPage from 'pages/ErrorPage';
+import LoadingPage from 'pages/LoadingPage';
+
+const IssuesPage = React.lazy(() => import('pages/IssuesPage'));
+const IssueDetailPage = React.lazy(() => import('pages/IssueDetailPage'));
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: (
+      <Suspense fallback={<LoadingPage />}>
+        <App />
+      </Suspense>
+    ),
     errorElement: <ErrorPage />,
     loader: rootLoader,
     children: [
